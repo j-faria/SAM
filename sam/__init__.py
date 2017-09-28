0542ebc4abae7aa8359f62ba3bb1aa114503dcef
João Faria <joao.faria@astro.up.pt>
2017-09-28 14:22:50 +0100

    add distributed noise; improved version string

--- sam/__init__.py
+++ sam/__init__.py
@@ -1,15 +1,31 @@
 """
 SAM: the Stellar Activity Machine
 """
+__all__ = ['Planet', 'Offset',
+           'WhiteNoise', 'DistributedNoise', 
+           'TimeSampling'
+          ]
 
-__all__ = ['Planet', 'WhiteNoise', 'Simulation']
-__version__ = '0.0.1'
+_version_major = 0
+_version_minor = 1
+_version_micro = 1  # use '' for first of series, number for 1 and above
+_version_extra = 'dev'  # use '' for full releases, 'dev' for development
 
-from _planet import Planet
-from _noise import WhiteNoise
+# Construct full version string
+_ver = [_version_major, _version_minor]
+if _version_micro:
+    _ver.append(_version_micro)
+if _version_extra:
+    _ver.append(_version_extra)
+__version__ = '.'.join(map(str, _ver))
+
+
+from _planet import Planet, Offset
+from _noise import WhiteNoise, DistributedNoise
 from _sampling import TimeSampling
 
 # units with Pint 
 from pint import UnitRegistry as _UnitRegistry
 units = _UnitRegistry()
 _Q = units.Quantity
+
--- sam/_noise.py
+++ sam/_noise.py
@@ -1,30 +1,73 @@
 import numpy as np
 import numpy.random as rng
-
+from scipy import stats
 from components import Component
 
+
 class WhiteNoise(Component):
-	def __init__(self, sd=None, var=None):
-		if sd is None and var is None:
-			sd, var = 1., 1.
-		elif sd is None:
-			var = float(var)
-			sd = np.sqrt(var)
-		elif var is None:
-			sd = float(sd)
-			var = sd**2
+    def __init__(self, sd=None, var=None):
+        if sd is None and var is None:
+            sd, var = 1., 1.
+        elif sd is None:
+            var = float(var)
+            sd = np.sqrt(var)
+        elif var is None:
+            sd = float(sd)
+            var = sd**2
 
-		self.sd = sd
-		self.var = var
-		self.random_state = rng.get_state()
+        self.sd = sd
+        self.var = var
+        self.random_state = rng.get_state()
+
+    def __repr__(self):
+        if self.sd < 1e-2:
+            return "WhiteNoise(sigma=%2.2e)" % self.sd
+        else:
+            return "WhiteNoise(sigma=%2.2f)" % self.sd
+
+    def sample(self, t=None, change_random_state=False):
+        if not change_random_state:
+            rng.set_state(self.random_state)
+        if t is None:
+            if self.sampling is None:
+                raise ValueError('provide `t` or use set_sampling')
+            t = self.sampling.get_times()
+
+        return rng.normal(loc=0., scale=self.sd, size=t.size)
 
 
-	def __repr__(self):
-		return "WhiteNoise(sigma=%2.2f)" % self.sd
+class DistributedNoise(Component):
+    def __init__(self, distribution, *args):
+        self.distribution = distribution
+        if isinstance(distribution, stats.rv_continuous):
+            self.frozen = False
+            self.args = args
+        elif isinstance(distribution, stats._distn_infrastructure.rv_frozen):
+            self.frozen = True
+            self.args = distribution.args
 
+        self.random_state = rng.get_state()
 
-	def sample(self, t, change_random_state=False):
-		if not change_random_state:
-			rng.set_state(self.random_state)
-		t = np.atleast_1d(t)
-		return rng.normal(loc=0., scale=self.sd, size=t.size)
+    def __repr__(self):
+        if self.frozen:
+            name = self.distribution.dist.name.capitalize()
+        else:
+            name = self.distribution.name.capitalize()
+
+        return "%sNoise(args=%s)" % \
+            (name, self.args)
+
+    def sample(self, t=None, change_random_state=False):
+        if t is None:
+            if self.sampling is None:
+                raise ValueError('provide `t` or use set_sampling')
+            t = self.sampling.get_times()
+
+        if not change_random_state:
+            rng.set_state(self.random_state)
+
+        if self.frozen:
+            return self.distribution.rvs(size=t.size)
+        else:
+            return self.distribution.rvs(*self.args, size=t.size)
+        # return rng.normal(loc=0., scale=self.sd, size=t.size)
