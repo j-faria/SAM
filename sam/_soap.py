71671c6e4802dfb2adb90ea65d14a41cf3abf6b4
jdavidrcamacho <jdavidrcamacho@gmail.com>
2017-12-11 08:37:31 +0000

    changes from last week

--- sam/_soap.py
+++ sam/_soap.py
@@ -1,28 +1,48 @@
+import warnings
 import numpy as np
 from components import Component
-
-from SOAP import Simulation
 from SOAP.classes import randomActReg
+
+try:
+	from SOAP import Simulation
+	SOAP_is_available = True
+except ImportError:
+	SOAP_is_available = False
+	with warnings.catch_warnings():
+		warnings.simplefilter("module")
+		warnings.warn('module SOAP could not be imported.', ImportWarning)
+
 __all__ = ['SOAP']
 
-class SOAP(Component):
-	def __init__(self, random=False):
-		self.sim = Simulation()
-		if random:
-			self.sim.active_regions = [randomActReg(),]
+if SOAP_is_available:
+
+	class SOAP(Component):
+		def __init__(self, random=False):
+			self.sim = Simulation()
+			if random:
+				self.sim.active_regions = [randomActReg(),]
 
 
-	def __repr__(self):
-		return self.sim.__repr__()
+
+		def __repr__(self):
+			return self.sim.__repr__()
+		def __condensed_repr__(self):
+			return self.sim.__repr__()
 
 
-	def sample(self, t):
-		psi = t / self.sim.star.prot
-		# try:
-		# 	if np.allclose(self.sim.PSI, psi): 
-		# 		return self.sim.rv_tot * 1e3
-		# except AttributeError:
-		# 	pass
+		def sample(self, t):
+			psi = t / self.sim.star.prot
+			# try:
+			# 	if np.allclose(self.sim.PSI, psi): 
+			# 		return self.sim.rv_tot * 1e3
+			# except AttributeError:
+			# 	pass
 
-		rv, _ = self.sim.calculate_activity_RV_signal(psi)
-		return rv * 1e3
+			rv, _ = self.sim.calculate_activity_RV_signal(psi)
+			return rv * 1e3
+
+else:
+
+	class SOAP(Component):
+		def __init__(self):
+			raise ValueError('SOAP is not available!')
