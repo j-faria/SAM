embrace astropy.units; fix granulation

sam/_granulation.py
@@ -1,29 +1,69 @@
 import numpy as np
 import numpy.random as rng
 from .components import Component
-from . import units, _Q
+from . import units, constants, ms
 from .utils import timeseries_from_power_spectrum, normalise_timeseries
+
+from astropy.stats import LombScargle
 
 __all__ = ['Granulation',]
 
 
+sigma_units = (ms**2/units.uHz)
+
 class Granulation(Component):
-    def __init__(self, sigma=75, tau=2/24, model='kallinger'):
+    
+    def __init__(self, sigma=0.01, tau=2, model='harvey', C=None):
+        try:
+            assert sigma.unit == sigma_units, \
+                'Units of `sigma` in Granulation should be %s' % sigma_units
+        except AttributeError:
+            sigma = sigma * sigma_units
+
+        tau_units = units.hour
+        try:
+            assert tau.unit == tau_units, \
+                'Units of `tau` in Granulation should be %s' % tau_units
+        except AttributeError:
+            tau = tau * tau_units
+
         self.sigma, self.tau = sigma, tau
+        if model.lower() not in ('harvey', 'kallinger'):
+            assert C is not None, \
+                'For a model that is not "harvey" or "kallinger", '\
+                'please provice the exponent `C`.'
+            self.C = C
+        self.model = model.lower()
+
         self.random_state = rng.get_state()
 
     def __repr__(self):
-        s = _Q(self.sigma, 'meter / second')
-        tau = _Q(self.tau, 'day')
-        return "Granulation(sigma={0:.2f~P}, tau={1:.2f~P})".format(s, tau)
+        s, tau = self.sigma, self.tau
+        return "Granulation(sigma={0:.3f}, tau={1:.3f})".format(s, tau)
 
     def __condensed_repr__(self):
-        return "KGran(%.1f, %.1f)" % (self.sigma, self.tau)
+        return "Gran(%.2f, %.2f)" % (self.sigma.value, self.tau.value)
 
-    def psd(self, f):
-        c = 2.0 * np.sqrt(2.0) / np.pi
-        b = 1 / (2*np.pi*self.tau)
-        return (c * self.sigma**2 / b) / (1.0 + (f / b) ** 4)
+    def psd_kallinger(self, nu, A, B):
+        assert A.unit == sigma_units
+        assert B.unit == units.hour
+        assert nu.unit == units.Hz
+        B = B.to(1/nu.unit)
+        return A / (1.0 + (B * nu) ** 4)
+
+    def psd_harvey(self, nu, A, B):
+        assert A.unit == sigma_units
+        assert B.unit == units.hour
+        assert nu.unit == units.Hz
+        B = B.to(1/nu.unit)
+        return A / (1.0 + (B * nu) ** 2)
+    
+    def psd_general(self, nu, A, B, C):
+        assert A.unit == sigma_units
+        assert B.unit == units.hour
+        assert nu.unit == units.Hz
+        B = B.to(1/nu.unit)
+        return A / (1.0 + (B * nu) ** C)
 
     # def eta_sq(self):
     #     """ Compute sinc^2 modulation from sampling """
@@ -38,15 +78,27 @@ class Granulation(Component):
                 raise ValueError('provide `t` or use set_sampling')
             t = self.sampling.get_times()
 
-        avg_Nyquist = 0.5 * len(t) / (t.max() - t.min())
-        f0 = 0.0
-        bw = 1.0 / t.ptp()
-        
-        freq = np.arange(f0, avg_Nyquist, bw)
-        power = self.psd(freq)
+        t = t * units.day
+        # freq comes in 1/d from autofrequency
+        freq = LombScargle(t, np.zeros_like(t)).autofrequency()
+        nu = freq.to(units.Hz)
 
-        y = timeseries_from_power_spectrum(freq, power)
-        y = normalise_timeseries(freq, power, t, y)
+        if self.model == 'harvey':
+            power = self.psd_harvey(nu, self.sigma, self.tau)
+        if self.model == 'kallinger':
+            power = self.psd_kallinger(nu, self.sigma, self.tau)
+        else:
+            power = self.psd_general(nu, self.sigma, self.tau, self.C)
+        
+        y = timeseries_from_power_spectrum(nu, power)
+        y = y[:t.size]
+
+        y = normalise_timeseries(nu, power, t, y)
+        
+        # in the middle of all this we lost a factor of 1000
+        # (the units of y are Hz(1/2) m / (s uHz(1/2))
+        factor = (units.Hz / units.uHz)**(1/2)
+        y = y * factor.decompose().scale
 
         return y
 

sam/_planet.py
@@ -9,7 +9,7 @@ from scipy import stats
 
 from . import kepler
 from .components import Component
-from . import units, _Q
+from . import units
 
 orb_pars = namedtuple('orbital_parameters', 'P K e omega Tp')
 pi = np.pi
@@ -153,9 +153,9 @@ class Planet(Component):
         if self.grid:
             return self._grid_par_repr()
         else:
-            P = _Q(self.P,'days')
-            K = _Q(self.K, 'meter / second')
-            e = _Q(self.e)
+            P = self.P #_Q(self.P,'days')
+            K = self.K #_Q(self.K, 'meter / second')
+            e = self.e #_Q(self.e)
             return "Planet(P={0:.2f~P}, K={1:.2f~P}, e={2:.2f~P})".format(P, K, e)
 
     """
@@ -198,9 +198,9 @@ class Planet(Component):
                 return var
 
         P, K, e = list(map(atleast_1d_new, [self.P, self.K, self.e]))
-        P = _Q(P,'days')
-        K = _Q(K, 'meter / second')
-        e = _Q(e)
+        P = self.P #_Q(P,'days')
+        K = self.K #_Q(K, 'meter / second')
+        e = self.e #_Q(e)
         s = "Planet(P={0:~P}, K={1:~P}, e={2:~P})".format(P, K, e)
 
         np.set_printoptions(**old_printoptions)

sam/utils.py
@@ -1,18 +1,25 @@
 import numpy as np
-from scipy.signal import periodogram
+from astropy.stats import LombScargle
 
-def power_spectrum(time, y):
-    time = time - time[0]
-    df = 1.0 / np.ediff1d(time).min()
-    f, p = periodogram(y, df)
+from . import units, ms
 
-    lhs = (1.0 / len(time)) * np.sum(y ** 2)
-    rhs = np.sum(p)
-    ratio = lhs / rhs
-    p *= ratio / df
-    fill = len(np.where(y != 0.0)[0]) / float(len(y))
-    p /= fill
-    return f, p
+
+def power_spectrum(t, y):
+    assert t.unit == units.day
+    assert y.unit == ms
+
+    f, p = LombScargle(t, y, normalization='psd').autopower()
+    N = t.size
+    # return frequency in Hz and power in (m/s)^2
+    return f.to(units.Hz), p/N
+
+
+def convolve_hanning(f, p, window):
+    # smooth the spectrum by convolving with a (normalized) Hann window of window points
+    kernel = np.hanning(window)
+    kernel = kernel / kernel.sum()
+    smoothed = np.convolve(kernel, p, mode='SAME')
+    return smoothed
 
 def timeseries_from_power_spectrum(freq, power):
     bw = freq[1] - freq[0]