224274b574ab925d0b108c7dcf009342666f3164
João Faria <joao.faria@astro.up.pt>
2018-09-07 18:43:15 +0100

    changes to SOAP, now it works

--- sam/_soap.py
+++ sam/_soap.py
@@ -1,10 +1,12 @@
 import warnings
+from copy import copy
 import numpy as np
 from .components import Component
 
 try:
 	from SOAP import Simulation
-	from SOAP.classes import randomActReg
+	from SOAP.classes import randomActReg, ActiveRegion
+	from SOAP.defaults import _default_ACTIVE_REGIONS, _default_STAR
 	SOAP_is_available = True
 except ImportError:
 	SOAP_is_available = False
@@ -16,24 +18,30 @@ __all__ = ['SOAP']
 
 if SOAP_is_available:
 
+	defaultAR = _default_ACTIVE_REGIONS[0]
+
 	class SOAP(Component):
-		def __init__(self, random=False):
-			self.sim = Simulation()
+		def __init__(self, Prot=25, lat=0, lon=0, size=0.1, type='spot',
+		             random=False):
+
+			artype = 0 if type.lower() == 'spot' else 1
+			ar = ActiveRegion(lon, lat, size, artype)
+			
+			self.simulation = Simulation(active_regions=[ar,])
+
 			if random:
-				self.sim.active_regions = [randomActReg(),]
-
-
-
+				self.simulation.active_regions = [randomActReg(),]
+			
 		def __repr__(self):
-			return self.sim.__repr__()
+			return self.simulation.__repr__()
 		def __condensed_repr__(self):
-			return self.sim.__repr__()
+			return self.simulation.__repr__()
 
 
-		def sample(self, t):
-			psi = t / self.sim.star.prot
+		def sample(self, t, *args):
+			psi = t / self.simulation.star.prot
 
-			rv, _ = self.sim.calculate_activity_RV_signal(psi)
+			_, rv = self.simulation.calculate_activity_signal(psi)
 			return (rv - rv.mean()) * 1e3
 
 else:
